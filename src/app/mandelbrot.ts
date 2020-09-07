1c7db795440a807cb743b93da08bb1b710fd6bcf
h3nnn4n <uber.renan@gmail.com>
2020-09-07 15:40:33 -0300

    Navigate using mouse / UI

--- src/app/mandelbrot.ts
+++ src/app/mandelbrot.ts
@@ -26,6 +26,28 @@ class Mandelbrot {
     this.y_max = this.y_center + this.zoom;
   }
 
+  public set_zoom(zoom: number) {
+    this.zoom = zoom;
+
+    this.update_bounds();
+  }
+
+  public set_new_coordinates(real: number, imag: number) {
+    this.x_center = real;
+    this.y_center = imag;
+
+    this.update_bounds();
+  }
+
+  public set_new_coordinates_from_mouse(mouse_x: number, mouse_y: number) {
+    const new_coords = this.mouse_to_mandelbrot(mouse_x, mouse_y);
+
+    this.x_center = new_coords.x;
+    this.y_center = new_coords.y;
+
+    this.update_bounds();
+  }
+
   public mouse_to_mandelbrot(mouse_x: number, mouse_y: number): any {
     const x = this.x_min + (this.x_max - this.x_min) * (mouse_x / this.canvas.width);
     const y = this.y_min + (this.y_max - this.y_min) * (mouse_y / this.canvas.height);
--- src/app/ui.ts
+++ src/app/ui.ts
@@ -22,14 +22,43 @@ class Ui {
 
         this.mouse_x = (event.clientX - rect.left) * scaleX;
         this.mouse_y = (event.clientY - rect.top) * scaleY;
+        this.mouse_y = canvas.height - this.mouse_y;
 
+        this.update_mouse_ui();
+      },
+    );
+
+    $('#main-canvas').click(
+      (event: any) => {
+        this.mandelbrot.set_new_coordinates_from_mouse(this.mouse_x, this.mouse_y);
         this.update_ui();
       },
     );
+
+    $('#real_value').change(() => this.update_mandelbrot_coordinates());
+    $('#imag_value').change(() => this.update_mandelbrot_coordinates());
+    $('#zoom_level').change(() => this.update_mandelbrot_zoom());
   }
 
-  private update_ui(): void {
-    const mandel_coords = this.mandelbrot.mouse_to_mandelbrot(this.mouse_x, this.mouse_y);
+  public update_ui(): void {
+    $('#real_value').val(this.mandelbrot.x_center);
+    $('#imag_value').val(this.mandelbrot.y_center);
+  }
+
+  private update_mandelbrot_zoom() {
+    this.mandelbrot.set_zoom($('#zoom_level').val() as number);
+  }
+
+  private update_mandelbrot_coordinates() {
+    this.mandelbrot.set_new_coordinates(
+      $('#real_value').val() as number,
+      $('#imag_value').val() as number,
+    );
+  }
+
+  private update_mouse_ui(): void {
+    const mandel_coords = this.mandelbrot.mouse_to_mandelbrot(this.mouse_x,
+                                                              this.mouse_y);
 
     $('#mouse_real_value').val(mandel_coords.x);
     $('#mouse_imag_value').val(mandel_coords.y);
